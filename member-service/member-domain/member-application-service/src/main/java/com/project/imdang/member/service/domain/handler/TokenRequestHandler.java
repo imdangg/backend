9bfc11779ea0f29a5579240081535e3395a42143
kyu-hyun <dlrbgus722@naver.com>
2024-11-22 16:13:52 +0900

    feat : 토큰 발급 handler

---
+++ member-service/member-domain/member-application-service/src/main/java/com/project/imdang/member/service/domain/handler/TokenRequestHandler.java
@@ -0,0 +1,46 @@
+package com.project.imdang.member.service.domain.handler;
+
+import com.project.imdang.member.service.domain.dto.TokenResponse;
+import com.project.imdang.member.service.domain.entity.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class TokenRequestHandler {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private static final String BEARER_TYPE = "Bearer";
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60;            // 60분
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+
+    public TokenResponse generate(Member member) {
+        long now = (new Date()).getTime();
+        Date accessTokenExpiredAt = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+        Date refreshTokenExpiredAt = new Date(now + REFRESH_TOKEN_EXPIRE_TIME);
+
+        String subject = member.getMemberId().getValue().toString();
+        String accessToken = jwtTokenProvider.generateAccessToken(subject, accessTokenExpiredAt);
+        log.info("AccessToken이 발급되었습니다. : {}", accessToken);
+
+        String refreshToken = jwtTokenProvider.generateRefreshToken(refreshTokenExpiredAt);
+        log.info("RefreshToken이 발급되었습니다. : {}", refreshToken);
+
+        return new TokenResponse(accessToken, refreshToken,ACCESS_TOKEN_EXPIRE_TIME / 1000L);
+    }
+
+    public UUID extractMemberId(String accessToken) {
+        //TODO : 토큰 유효성 검사
+       return UUID.fromString(jwtTokenProvider.extractSubject(accessToken));
+    }
+
+    public void storeRefreshToken(String oAuthId, String refreshToken) {
+
+    }
+}
