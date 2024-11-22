ae2208adb7dbf0fe1c8c69035cd0923052baf643
kyu-hyun <dlrbgus722@naver.com>
2024-11-22 16:21:12 +0900

    refactor : Member Domain 온보딩 관련 필드 추가

--- member-service/member-domain/member-domain-core/src/main/java/com/project/imdang/member/service/domain/entity/Member.java
+++ member-service/member-domain/member-domain-core/src/main/java/com/project/imdang/member/service/domain/entity/Member.java
@@ -3,28 +3,46 @@ package com.project.imdang.member.service.domain.entity;
 import com.project.imdang.domain.entity.AggregateRoot;
 import com.project.imdang.domain.valueobject.MemberId;
 import com.project.imdang.member.service.domain.valueobject.Gender;
+import com.project.imdang.member.service.domain.valueobject.OAuthType;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class Member extends AggregateRoot<MemberId> {
 
 //    private String name;
+    private MemberId memberId;
     private String nickname;
     private String birthDate;
     private Gender gender;
-    private String image;
+//    private String image;
+
+    private String oAuthId;
+    private OAuthType oAuthType;
 
 //    private List<InsightId> insightIds;
 //    private int insightCount;
     private int exchangeCount;
 
-    public Member(String nickname, String birthDate, Gender gender, String image) {
+    @Builder
+    public Member(MemberId memberId, String nickname, String birthDate, Gender gender, String oAuthId, OAuthType oAuthType) {
+        this.memberId = memberId;
         this.nickname = nickname;
         this.birthDate = birthDate;
         this.gender = gender;
-        this.image = image;
+        this.oAuthId = oAuthId;
+        this.oAuthType = oAuthType;
         this.exchangeCount = 0;
     }
 
-/*
+    public Member join(String nickname, String birthDate, String gender) {
+        this.nickname = nickname;
+        this.birthDate = birthDate;
+        this.gender = Gender.getGenderType(gender);
+        return this;
+    }
+
+    /*
     private void setName(String name) {
         if (name.length() < 1) {
             throw new NameNotEnoughLengthException();
@@ -32,7 +50,9 @@ public class Member extends AggregateRoot<MemberId> {
         this.name = name;
     }*/
 
+    /*
     private void setImage(String image) {
         this.image = image;
     }
+    */
 }
