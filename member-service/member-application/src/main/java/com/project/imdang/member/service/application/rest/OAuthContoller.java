411560e0d4d724f9ae9b93f739874d9d20edb9e0
kyu-hyun <dlrbgus722@naver.com>
2024-11-22 16:09:37 +0900

    refactor : 응답 DTO 변경

--- member-service/member-application/src/main/java/com/project/imdang/member/service/application/rest/OAuthContoller.java
+++ member-service/member-application/src/main/java/com/project/imdang/member/service/application/rest/OAuthContoller.java
@@ -1,16 +1,17 @@
 package com.project.imdang.member.service.application.rest;
 
+import com.project.imdang.member.service.domain.dto.LoginResponse;
 import com.project.imdang.member.service.domain.dto.oauth.apple.AppleLoginParamsCommand;
 import com.project.imdang.member.service.domain.dto.oauth.google.GoogleLoginParamsCommand;
 import com.project.imdang.member.service.domain.dto.oauth.kakao.KakaoLoginParamsCommand;
 import com.project.imdang.member.service.domain.port.input.service.OAuthLoginService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class OAuthContoller {
 
     private final OAuthLoginService oAuthLoginService;
@@ -19,24 +20,27 @@ public class OAuthContoller {
      * 카카오 로그인
      */
     @PostMapping("/kakao")
-    public void login(@RequestBody KakaoLoginParamsCommand kakaoLoginCommand) {
-        oAuthLoginService.login(kakaoLoginCommand);
+    public ResponseEntity<?> login(@RequestBody KakaoLoginParamsCommand kakaoLoginCommand) {
+        LoginResponse response = oAuthLoginService.login(kakaoLoginCommand);
+        return ResponseEntity.ok(response);
     }
 
     /**
      * 애플 로그인
      */
     @PostMapping("/apple")
-    public void login(@RequestBody AppleLoginParamsCommand appleLoginCommand) {
-        oAuthLoginService.login(appleLoginCommand);
+    public ResponseEntity<?> login(@RequestBody AppleLoginParamsCommand appleLoginCommand) {
+        LoginResponse response = oAuthLoginService.login(appleLoginCommand);
+        return ResponseEntity.ok(response);
     }
 
     /**
      * 구글 로그인
      */
     @PostMapping("/google")
-    public void login(@RequestBody GoogleLoginParamsCommand googleLoginCommand) {
-        oAuthLoginService.login(googleLoginCommand);
+    public ResponseEntity<?> login(@RequestBody GoogleLoginParamsCommand googleLoginCommand) {
+        LoginResponse response = oAuthLoginService.login(googleLoginCommand);
+        return ResponseEntity.ok(response);
     }
 
 
