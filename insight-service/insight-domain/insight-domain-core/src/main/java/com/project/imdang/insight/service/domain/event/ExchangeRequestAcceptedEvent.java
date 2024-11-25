fc8b165df206acbb4caac60846fdbcdc9479c212
kyu-hyun <dlrbgus722@naver.com>
2024-11-25 17:03:43 +0900

    feat : 수락, 거절 event 필드 추가

--- insight-service/insight-domain/insight-domain-core/src/main/java/com/project/imdang/insight/service/domain/event/ExchangeRequestAcceptedEvent.java
+++ insight-service/insight-domain/insight-domain-core/src/main/java/com/project/imdang/insight/service/domain/event/ExchangeRequestAcceptedEvent.java
@@ -1,4 +1,10 @@
 package com.project.imdang.insight.service.domain.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class ExchangeRequestAcceptedEvent {
+    private String memberId;
 }
