cde622315a18d5fcadcfdf2ac4ef3dead05d18c1
kyu-hyun <dlrbgus722@naver.com>
2024-11-25 17:08:38 +0900

    feat : 내가 교환 요청 & 나에게 교환 요청 조회 persistence 관련

--- insight-service/insight-domain/insight-application-service/src/main/java/com/project/imdang/insight/service/domain/ports/output/repository/ExchangeRequestRepository.java
+++ insight-service/insight-domain/insight-application-service/src/main/java/com/project/imdang/insight/service/domain/ports/output/repository/ExchangeRequestRepository.java
@@ -1,16 +1,20 @@
 package com.project.imdang.insight.service.domain.ports.output.repository;
 
 import com.project.imdang.domain.valueobject.ExchangeRequestId;
+import com.project.imdang.domain.valueobject.MemberId;
 import com.project.imdang.insight.service.domain.entity.ExchangeRequest;
+
+import java.util.List;
 
 public interface ExchangeRequestRepository {
 
     ExchangeRequest save(ExchangeRequest exchangeRequest);
-//    ExchangeRequest update(ExchangeRequest exchangeRequest);
     // TODO - CHECK : 교환요청 삭제
     void deleteById(ExchangeRequestId exchangeRequestId);
 
-//    Optional<ExchangeRequest> findById(ExchangeRequestId exchangeRequestId);
-//    List<ExchangeRequest> findAll();
     ExchangeRequest findExchangeRequest(ExchangeRequestId requestId);
+
+    List<ExchangeRequest> findAllByMe(MemberId memberId);
+
+    List<ExchangeRequest> findAllByOther(MemberId memberId);
 }
--- insight-service/insight-persistence/src/main/java/com/project/imdang/insight/service/persistence/insight/adapter/ExchangeRequestRepositoryImpl.java
+++ insight-service/insight-persistence/src/main/java/com/project/imdang/insight/service/persistence/insight/adapter/ExchangeRequestRepositoryImpl.java
@@ -1,16 +1,24 @@
 package com.project.imdang.insight.service.persistence.insight.adapter;
 
 import com.project.imdang.domain.valueobject.ExchangeRequestId;
+import com.project.imdang.domain.valueobject.MemberId;
 import com.project.imdang.insight.service.domain.entity.ExchangeRequest;
 import com.project.imdang.insight.service.domain.ports.output.repository.ExchangeRequestRepository;
 import com.project.imdang.insight.service.persistence.insight.entity.ExchangeRequestEntity;
 import com.project.imdang.insight.service.persistence.insight.mapper.ExchangeRequestPersistenceMapper;
 import com.project.imdang.insight.service.persistence.insight.repository.ExchangeRequestJpaRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
+@Slf4j
 @Component
 public class ExchangeRequestRepositoryImpl implements ExchangeRequestRepository {
 
@@ -37,5 +45,30 @@ public class ExchangeRequestRepositoryImpl implements ExchangeRequestRepository 
         ExchangeRequestEntity findExchangeRequest = exchangeRequestJpaRepository.findById(requestId.getValue())
                 .orElseThrow(() -> new NoSuchElementException("일치하는 교환요청이 존재하지 않습니다."));
         return exchangeRequestPersistenceMapper.exchangeEntityToExchange(findExchangeRequest);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<ExchangeRequest> findAllByMe(MemberId memberId) {
+        List<ExchangeRequestEntity> findExchangeRequestEntity = exchangeRequestJpaRepository.findAllByRequestMemberId(memberId.getValue())
+                .orElse(new ArrayList<>());
+        log.info("{} 가 요청한 교환 요청 갯수 : {}", memberId.getValue(), findExchangeRequestEntity.size());
+
+        return findExchangeRequestEntity.stream()
+                .map(exchangeRequestPersistenceMapper::exchangeEntityToExchange)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<ExchangeRequest> findAllByOther(MemberId memberId) {
+        // 엔티티 중 요청한 인사이트 ID 속 멤버ID가 == memberId
+        List<ExchangeRequestEntity> findExchangeRequestEntity = exchangeRequestJpaRepository.findAllOtherByRequestMemberId(memberId.getValue())
+                .orElse(new ArrayList<>());
+        log.info("{} 에게 요청한 교환 요청 갯수 : {}", memberId.getValue(), findExchangeRequestEntity.size());
+
+        return findExchangeRequestEntity.stream()
+                .map(exchangeRequestPersistenceMapper::exchangeEntityToExchange)
+                .collect(Collectors.toList());
     }
 }
