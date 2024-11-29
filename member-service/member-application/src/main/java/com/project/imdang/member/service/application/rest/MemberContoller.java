c9d1968e693bad5790c23297833bbd7bef5f02d2
kyu-hyun <dlrbgus722@naver.com>
2024-11-30 01:01:32 +0900

    refactor : Join Service 클래스 삭제

--- member-service/member-application/src/main/java/com/project/imdang/member/service/application/rest/MemberContoller.java
+++ member-service/member-application/src/main/java/com/project/imdang/member/service/application/rest/MemberContoller.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 public class MemberContoller {
 
     private final MemberService memberService;
-    private final JoinService joinService;
 
     /**
      * 카카오 로그인
--- member-service/member-domain/member-application-service/src/main/java/com/project/imdang/member/service/domain/port/input/service/JoinService.java
+++
@@ -1,9 +0,0 @@
-package com.project.imdang.member.service.domain.port.input.service;
-
-import com.project.imdang.member.service.domain.dto.JoinCommand;
-import com.project.imdang.member.service.domain.dto.LoginResponse;
-
-
-public interface JoinService {
-    LoginResponse join(String accessToken, JoinCommand joinCommand);
-}
