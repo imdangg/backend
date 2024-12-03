625c9528bf91703d7e69274a963d4b4ae6674a94
kyu-hyun <dlrbgus722@naver.com>
2024-12-03 15:28:28 +0900

    refactor : 교환 요청 DTO 타입 수정

--- insight-service/insight-domain/insight-application-service/src/main/java/com/project/imdang/insight/service/domain/dto/exchange/request/RequestExchangeInsightResponse.java
+++ insight-service/insight-domain/insight-application-service/src/main/java/com/project/imdang/insight/service/domain/dto/exchange/request/RequestExchangeInsightResponse.java
@@ -2,12 +2,14 @@ package com.project.imdang.insight.service.domain.dto.exchange.request;
 
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 public class RequestExchangeInsightResponse {
     //교환 ID
-    private final String exchangeId;
+    private final UUID exchangeId;
 
-    public RequestExchangeInsightResponse(String exchangeId) {
+    public RequestExchangeInsightResponse(UUID exchangeId) {
         this.exchangeId = exchangeId;
     }
 }
