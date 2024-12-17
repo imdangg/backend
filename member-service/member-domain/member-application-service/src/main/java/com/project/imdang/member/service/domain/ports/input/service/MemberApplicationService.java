b5b47ce7b54355eec705f129c4481fa4498c08ec
kyu-hyun <dlrbgus722@naver.com>
2024-12-18 01:28:17 +0900

    refactor: 기존 코드 변경

--- member-service/member-domain/member-application-service/src/main/java/com/project/imdang/member/service/domain/ports/input/service/MemberApplicationService.java
+++ member-service/member-domain/member-application-service/src/main/java/com/project/imdang/member/service/domain/ports/input/service/MemberApplicationService.java
@@ -2,10 +2,15 @@ package com.project.imdang.member.service.domain.ports.input.service;
 
 import com.project.imdang.member.service.domain.dto.JoinCommand;
 import com.project.imdang.member.service.domain.dto.LoginResponse;
+import com.project.imdang.member.service.domain.dto.TokenResponse;
 import com.project.imdang.member.service.domain.dto.oauth.OAuthLoginCommand;
 import com.project.imdang.member.service.domain.entity.Member;
 
+import java.util.UUID;
+
 public interface MemberApplicationService {
     LoginResponse login(OAuthLoginCommand loginCommand);
-    void join(String accessToken, JoinCommand joinCommand);
+    void join(UUID memberId, JoinCommand joinCommand);
+
+    TokenResponse test();
 }
