d424b1ee782f7cab9d9a587780ebd0e5dc3699fd
kyu-hyun <dlrbgus722@naver.com>
2024-12-18 12:51:10 +0900

    docs: 약관, 알림 swagger 적용

--- setting-service/setting-application/src/main/java/com/project/imdang/setting/service/application/rest/NotificationController.java
+++ setting-service/setting-application/src/main/java/com/project/imdang/setting/service/application/rest/NotificationController.java
@@ -3,18 +3,21 @@ package com.project.imdang.setting.service.application.rest;
 import com.project.imdang.setting.service.domain.dto.ListNotificationQuery;
 import com.project.imdang.setting.service.domain.dto.NotificationResponse;
 import com.project.imdang.setting.service.domain.ports.input.service.NotificationApplicationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
+@Tag(name = "NotificationController", description = "알림 API")
 @RequestMapping("/notifications")
 @RequiredArgsConstructor
 @RestController
@@ -24,6 +27,8 @@ public class NotificationController {
 
     // 읽지 않은 알림 리스트 조회 (+ '읽음' 상태로 변경)
     // notifications/unchecked
+    @Operation(description = "읽지 않은 알림 목록 조회 API")
+    @ApiResponse(responseCode = "200", description = "읽지 않은 알림 목록 조회 성공")
     @GetMapping("/unchecked")
     public ResponseEntity<Page<NotificationResponse>> listUnchecked(@ModelAttribute ListNotificationQuery listNotificationQuery) {
         Page<NotificationResponse> paged = notificationApplicationService.listUncheckedNotification(listNotificationQuery);
--- setting-service/setting-application/src/main/java/com/project/imdang/setting/service/application/rest/TermsController.java
+++ setting-service/setting-application/src/main/java/com/project/imdang/setting/service/application/rest/TermsController.java
@@ -1,8 +1,14 @@
 package com.project.imdang.setting.service.application.rest;
 
 import com.project.imdang.setting.service.domain.dto.AgreeTermsCommand;
+import com.project.imdang.setting.service.domain.dto.NotificationResponse;
 import com.project.imdang.setting.service.domain.dto.TermsResponse;
 import com.project.imdang.setting.service.domain.ports.input.service.TermsApplicationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Slf4j
+@Tag(name = "TermsController", description = "약관 API")
 @RequestMapping("/terms")
 @RequiredArgsConstructor
 @RestController
@@ -22,12 +29,17 @@ public class TermsController {
 
     private final TermsApplicationService termsApplicationService;
 
+    @Operation(description = "약관 목록 조회 API")
+    @ApiResponse(responseCode = "200", description = "약관 목록 조회 성공",
+            content = @Content(schema = @Schema(implementation = TermsResponse.class)))
     @GetMapping
     public ResponseEntity<List<TermsResponse>> list() {
         List<TermsResponse> list = termsApplicationService.listTerms();
         return ResponseEntity.ok(list);
     }
 
+    @Operation(description = "약관 동의 API")
+    @ApiResponse(responseCode = "200", description = "약관 동의 성공")
     @PostMapping("/agree")
     public ResponseEntity<Void> agree(@RequestBody AgreeTermsCommand agreeTermsCommand) {
         termsApplicationService.agreeTerms(agreeTermsCommand);
