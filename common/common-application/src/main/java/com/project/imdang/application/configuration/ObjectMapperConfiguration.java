26049f89ccd4be9cd997bd2f33ec156fd79a756c
kyu-hyun <dlrbgus722@naver.com>
2024-12-30 16:33:30 +0900

    refactor : objectmapper 중복 해결

---
+++ common/common-application/src/main/java/com/project/imdang/application/configuration/ObjectMapperConfiguration.java
@@ -0,0 +1,18 @@
+package com.project.imdang.application.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("objectMapperConfiguration")
+public class ObjectMapperConfiguration {
+
+    @Bean
+    public ObjectMapper insightObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule()); // Java 8 날짜/시간 모듈 등록
+        objectMapper.configure(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false); // ISO 8601 포맷 사용
+        return objectMapper;
+    }
+}
