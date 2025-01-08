e9294eb50153048b4964e5f4ce2bdc2b120a96b8
kyu-hyun <dlrbgus722@naver.com>
2025-01-08 12:54:01 +0900

    fix : token 오류 수정

--- common/common-domain/common-application-service/src/main/java/com/project/imdang/domain/jwt/JwtTokenProvider.java
+++ common/common-domain/common-application-service/src/main/java/com/project/imdang/domain/jwt/JwtTokenProvider.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
-import java.util.UUID;
 
 // TODO - 클래스 이동
 @Slf4j
@@ -56,7 +55,7 @@ public final class JwtTokenProvider implements InitializingBean {
     public boolean verifyToken(String token) {
         try {
             Jws<Claims> claims = Jwts.parser()
-                    .setSigningKey(secretKey).build()
+                    .setSigningKey(key).build()
                     .parseClaimsJws(token);
 
             return claims.getBody().getExpiration().after(new Date());
