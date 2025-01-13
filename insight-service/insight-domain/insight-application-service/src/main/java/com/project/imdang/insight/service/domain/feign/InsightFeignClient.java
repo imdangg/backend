b8fe6fc0b010d22e1a68e41cdf1ab735615c65c0
kyu-hyun <dlrbgus722@naver.com>
2025-01-13 18:02:16 +0900

    feat : feign 설정

---
+++ insight-service/insight-domain/insight-application-service/src/main/java/com/project/imdang/insight/service/domain/feign/InsightFeignClient.java
@@ -0,0 +1,15 @@
+package com.project.imdang.insight.service.domain.feign;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.UUID;
+
+@FeignClient(name = "insightFeignClient", url = "http://localhost:8080/members")
+public interface InsightFeignClient {
+
+    @GetMapping
+    MemberInfoResponse getMemberInfo(@RequestParam UUID memberId);
+}
