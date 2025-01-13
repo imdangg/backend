62737de19df0426fd76ea2c5fd405b603a65388f
kyu-hyun <dlrbgus722@naver.com>
2025-01-13 18:04:07 +0900

    feat : security IP 제한 추가

--- common/common-application/src/main/java/com/project/imdang/application/configuration/SecurityConfiguration.java
+++ common/common-application/src/main/java/com/project/imdang/application/configuration/SecurityConfiguration.java
@@ -3,20 +3,33 @@ package com.project.imdang.application.configuration;
 import com.project.imdang.application.security.JwtAuthenticationEntryPoint;
 import com.project.imdang.application.security.JwtAuthFilter;
 import com.project.imdang.domain.jwt.JwtTokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
+
+import java.util.function.Supplier;
 
 @Configuration("securityConfiguration")
 @EnableWebSecurity(debug = true)
 @RequiredArgsConstructor
+@Slf4j
 public class SecurityConfiguration {
 
+    @Value("${security.allowed-ip}")
+    private String ALLOWED_IP_ADDRESS_6;
     private final JwtTokenProvider jwtTokenUtil;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
@@ -33,10 +46,16 @@ public class SecurityConfiguration {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/kakao", "/auth/google", "/auth/apple", "/members",
+                        .requestMatchers("/members").access(this::hasIpAddress)
+                        .requestMatchers("/auth/kakao", "/auth/google", "/auth/apple",
                                 "/swagger-resources/**", "/swagger-ui/**","/v3/api-docs/**", "/apartment-complexes").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(new JwtAuthFilter(jwtTokenUtil), UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    private AuthorizationDecision hasIpAddress(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
+        log.info("Local Request[IP : {}] is requested", object.getRequest().getRemoteAddr());
+        return new AuthorizationDecision(ALLOWED_IP_ADDRESS_6.matches(object.getRequest().getRemoteAddr()));
     }
 }
