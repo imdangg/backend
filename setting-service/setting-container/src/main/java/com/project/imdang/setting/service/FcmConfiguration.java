2e4e4292bd82379d6c108e0296c87937bf09ed4c
DESKTOP-0N7JBUG\user <younkyong94@gmail.com>
2025-01-26 10:14:48 +0900

    FCM 수정

--- setting-service/setting-container/src/main/java/com/project/imdang/setting/service/FcmConfiguration.java
+++ setting-service/setting-container/src/main/java/com/project/imdang/setting/service/FcmConfiguration.java
@@ -1,4 +1,4 @@
-package com.project.imdang.setting.service.domain;
+package com.project.imdang.setting.service;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
