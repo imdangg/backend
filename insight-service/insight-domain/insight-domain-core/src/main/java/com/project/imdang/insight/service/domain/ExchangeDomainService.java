052edb3e740740fcb3dc6785738e0c53eb387a3e
kyu-hyun <dlrbgus722@naver.com>
2025-02-22 02:19:55 +0900

    fix : 푸시알림 이모티콘 수정 및 쿠폰 상황 추가

--- insight-service/insight-domain/insight-domain-core/src/main/java/com/project/imdang/insight/service/domain/ExchangeDomainService.java
+++ insight-service/insight-domain/insight-domain-core/src/main/java/com/project/imdang/insight/service/domain/ExchangeDomainService.java
@@ -4,12 +4,13 @@ import com.project.imdang.domain.valueobject.MemberCouponId;
 import com.project.imdang.insight.service.domain.entity.ExchangeRequest;
 import com.project.imdang.insight.service.domain.entity.Snapshot;
 import com.project.imdang.insight.service.domain.event.ExchangeRequestAcceptedEvent;
+import com.project.imdang.insight.service.domain.event.ExchangeRequestByCouponCreatedEvent;
 import com.project.imdang.insight.service.domain.event.ExchangeRequestCreatedEvent;
 import com.project.imdang.insight.service.domain.event.ExchangeRequestRejectedEvent;
 
 public interface ExchangeDomainService {
     ExchangeRequestCreatedEvent requestExchange(ExchangeRequest exchangeRequest, Snapshot requestedSnapshot, Snapshot requestMemberSnapshot);
-    ExchangeRequestCreatedEvent requestExchangeWithCoupon(ExchangeRequest exchangeRequest, Snapshot requestedSnapshot, MemberCouponId memberCouponId);
+    ExchangeRequestByCouponCreatedEvent requestExchangeWithCoupon(ExchangeRequest exchangeRequest, Snapshot requestedSnapshot, MemberCouponId memberCouponId);
     ExchangeRequestAcceptedEvent acceptExchangeRequest(ExchangeRequest exchangeRequest);
     ExchangeRequestRejectedEvent rejectExchangeRequest(ExchangeRequest exchangeRequest);
 }
