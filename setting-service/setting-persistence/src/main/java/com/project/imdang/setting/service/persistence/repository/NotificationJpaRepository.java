4121baac5b67e6baf1b7aa01c9b2ca6d5a965634
kyu-hyun <dlrbgus722@naver.com>
2025-04-01 22:57:29 +0900

    fix

--- setting-service/setting-persistence/src/main/java/com/project/imdang/setting/service/persistence/repository/NotificationJpaRepository.java
+++ setting-service/setting-persistence/src/main/java/com/project/imdang/setting/service/persistence/repository/NotificationJpaRepository.java
@@ -4,6 +4,7 @@ import com.project.imdang.setting.service.persistence.entity.NotificationEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.ZonedDateTime;
@@ -12,6 +13,10 @@ import java.util.UUID;
 
 @Repository
 public interface NotificationJpaRepository extends JpaRepository<NotificationEntity, Long> {
+
+    @Query(value = "SELECT * FROM notification n  WHERE n.receiver_id = :receiverId AND n.is_checked = :checked AND n.created_at > :time \n-- #pageRequest\n",
+            countQuery = "SELECT count(*) FROM notification n  WHERE n.receiver_id = :receiverId AND n.is_checked = :checked AND n.created_at > :time",
+            nativeQuery = true)
     Page<NotificationEntity> findAllByReceiverIdAndIsCheckedAndCreatedAtAfter(UUID receiverId, Boolean checked, ZonedDateTime time, PageRequest pageRequest);
     List<NotificationEntity> findAllByReceiverIdAndIsChecked(UUID memberId, Boolean checked);
 }
