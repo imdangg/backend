6c10dc81888839b05fbdb2db49bd89bdaeecd1d2
kyu-hyun <dlrbgus722@naver.com>
2025-04-17 00:40:40 +0900

    fix

--- setting-service/setting-domain/setting-application-service/src/main/java/com/project/imdang/setting/service/domain/SendNotificationListener.java
+++ setting-service/setting-domain/setting-application-service/src/main/java/com/project/imdang/setting/service/domain/SendNotificationListener.java
@@ -38,25 +38,9 @@ public class SendNotificationListener implements EventListener {
         try {
 
             NotificationCategory category = NotificationCategory.getType(eventEntry.getType());
-            // 요청한 사람
-            UUID requestMemberId = getMemberId(eventEntry.getPayload(), "requestMemberId");
-            // 받는 사람
-            UUID requestedMemberId = getMemberId(eventEntry.getPayload(), "requestedMemberId");
-            UUID receiverId;
-            UUID senderId;
+            UUID receiverId = getReceiverId(eventEntry.getPayload());
 
-            //1. 교환 요청 -> 요청 받은 사람에게
-            if (category.equals(NotificationCategory.REQUESTED) || category.equals(NotificationCategory.REQUESTED_BY_COUPON)) {
-                senderId = requestMemberId;
-                receiverId = requestedMemberId;
-            }
-            // 2. 승인 및 거절은 요청한 사람에게
-            else {
-                senderId = requestedMemberId;
-                receiverId = requestMemberId;
-            }
-
-            Optional<MemberInfo> memberInfo = settingMemberLookup.lookupByMemberId(new MemberId(senderId));
+            Optional<MemberInfo> memberInfo = settingMemberLookup.lookupByMemberId(new MemberId(receiverId));
             if (memberInfo.isPresent()) {
                 String message = String.format(category.getNotificationContent(), memberInfo.get().nickname());
 
@@ -74,11 +58,11 @@ public class SendNotificationListener implements EventListener {
         }
     }
 
-    private UUID getMemberId(String payload, String target) {
+    private UUID getReceiverId(String payload) {
         try {
             JsonNode jsonNode = objectMapper.readTree(payload);
-            String memberId = jsonNode.get("exchangeRequest").get(target).get("value").asText();
-            return UUID.fromString(memberId);
+            String receiverId = jsonNode.get("exchangeRequest").get("requestedMemberId").get("value").asText();
+            return UUID.fromString(receiverId);
         } catch (JsonProcessingException e) {
             // TODO - 예외 처리
             throw new DomainException("JSON ERROR!");
