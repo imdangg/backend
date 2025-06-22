2b7d8f1c4c611adbc30a0bea51b52ae13e4dcef6
kyu-hyun <dlrbgus722@naver.com>
2025-06-22 22:08:09 +0900

    fix : DistrictController 관련

--- insight-service/insight-application/src/main/java/com/project/imdang/insight/application/rest/DistrictController.java
+++ insight-service/insight-application/src/main/java/com/project/imdang/insight/application/rest/DistrictController.java
@@ -1,10 +1,12 @@
 package com.project.imdang.insight.application.rest;
 
+import com.project.imdang.common.application.response.ApiResponse;
 import com.project.imdang.insight.domain.dto.insight.list.DistrictResult;
 import com.project.imdang.insight.domain.ports.input.service.InsightApplicationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +23,11 @@ public class DistrictController {
     // TODO - 캐싱
     private final InsightApplicationService insightApplicationService;
 
+    /**
+     * 시군구 목록 조회 API
+     */
     @GetMapping("/si-gun-gu")
-    public ResponseEntity<Page<DistrictResult>> listSiGunGu(
+    public ResponseEntity<ApiResponse<Page<DistrictResult>>> listSiGunGu(
             @RequestParam(name = "pageNumber", defaultValue = "0") Integer pageNumber,
             @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
         Page<DistrictResult> districts = insightApplicationService.listDistrict(DEFAULT_SI_DO, null, pageNumber, pageSize)
@@ -32,11 +37,14 @@ public class DistrictController {
                         .eupMyeonDong(district.getEupMyeonDong())
                         .code(district.getCode())
                         .build());
-        return ResponseEntity.ok(districts);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(districts));
     }
 
+    /**
+     * 읍면동 목록 조회 API
+     */
     @GetMapping("/eup-myeon-dong")
-    public ResponseEntity<Page<DistrictResult>> listEupMyeonDong(
+    public ResponseEntity<ApiResponse<Page<DistrictResult>>> listEupMyeonDong(
             @RequestParam(name = "siGunGu") String siGunGu,
             @RequestParam(name = "pageNumber", defaultValue = "0") Integer pageNumber,
             @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
@@ -47,6 +55,6 @@ public class DistrictController {
                         .eupMyeonDong(district.getEupMyeonDong())
                         .code(district.getCode())
                         .build());
-        return ResponseEntity.ok(districts);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(districts));
     }
 }
