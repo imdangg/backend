f27e9827945c0af2b4dfe58b0e9188d03286df2d
khlee <khlee@mostisoft.com>
2025-07-29 11:13:27 +0900

    test : 조건/우선순위 설정 테스트 (실거주)

--- imdang-container/src/test/java/com/project/imdang/member/MemberControllerTest.java
+++ imdang-container/src/test/java/com/project/imdang/member/MemberControllerTest.java
@@ -1,5 +1,6 @@
 package com.project.imdang.member;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.imdang.common.domain.valueobject.MemberId;
 import com.project.imdang.member.domain.ports.output.repository.MemberRepository;
 import com.project.imdang.member.persistence.provider.JwtTokenProvider;
@@ -13,24 +14,27 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import static com.project.imdang.member.TestData.accessToken;
 import static com.project.imdang.member.TestData.member;
 import static com.project.imdang.member.TestData.memberId;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
-@SpringBootTest(classes = TestConfiguration.class)
+@SpringBootTest
 public class MemberControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     private MemberRepository memberRepository;
 
     @MockBean
@@ -56,6 +60,50 @@ public class MemberControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.nickname").value("imdang"))
                 .andExpect(jsonPath("$.deviceToken").value("fcm-token"))
+                .andDo(print())
+                .andReturn();
+    }
+
+    @Test
+    void condition() throws Exception {
+        //given
+        Mockito.doNothing().when(jwtTokenProvider).validate(accessToken);
+
+        Map<String, Object> mockClaims = new HashMap<>();
+        mockClaims.put("memberId", memberId);
+
+        Mockito.when(jwtTokenProvider.extractClaims(accessToken))
+                .thenReturn(mockClaims);
+
+        String requestBody = """
+        {
+            "purpose": "실거주",
+            "budget": "1억 이하",
+            "monthIncome": "수입 없음",
+            "livingPerson": "중년부부",
+            "childrenPlan": "자녀 계획 있음",
+            "hopeGap": null,
+            "investmentPlan": null,
+            "traffic": "역세권",
+            "schoolDistrict": "학원가 근접",
+            "apartmentSquare": null,
+            "household": null,
+            "houseType": null,
+            "commutingArea": "여의도",
+            "infra": "대형병원",
+            "environment": "공원",
+            "firstPriority": "출퇴근지역,여의도",
+            "secondPriority": "학군,학원가 근접",
+            "thirdPriority": "교통,역세권"
+        }
+        """;
+
+        //then
+        mockMvc.perform(post("/members/condition")
+                        .header("Authorization", "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
                 .andDo(print())
                 .andReturn();
     }
