52d10ca3d061785966a0db6145ef52970479dd75
khlee <khlee@mostisoft.com>
2025-07-29 11:12:35 +0900

    feat : enum value 매핑 함수 구현

--- common/common-domain/common-domain-core/src/main/java/com/project/imdang/common/domain/valueobject/gapInvestment/HouseType.java
+++ common/common-domain/common-domain-core/src/main/java/com/project/imdang/common/domain/valueobject/gapInvestment/HouseType.java
@@ -1,7 +1,11 @@
 package com.project.imdang.common.domain.valueobject.gapInvestment;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
 
 /**
  * 유형
@@ -11,4 +15,17 @@ import lombok.RequiredArgsConstructor;
 public enum HouseType {
     NEW("신축"), OLD("구축");
     private final String value;
+
+    @JsonCreator
+    public static HouseType fromValue(String value) {
+        return Arrays.stream(HouseType.values())
+                .filter(ht -> ht.getValue().equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Unknown value: " + value));
+    }
+
+    @JsonValue
+    public String toValue() {
+        return value;
+    }
 }
